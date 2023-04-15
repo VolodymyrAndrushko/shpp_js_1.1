c96b480097042d86f5cca3e8185dcf47a0a51fea
VolodymyrAndrushko <mrkamilot@gmial.com>
2023-04-15 17:40:31 +0300

    Finished 3.1

--- 3.1/dist/index.js
+++ 3.1/dist/index.js
@@ -44,4 +44,73 @@
 // app.listen(3000, ()=>{
 //     console.log("Server started at port 3000")
 // })
+// //task 3 bug fix
+// interface isBigObject {
+//     // field: {cvalue:number} | {cvalue:string} | {cvalue: isBigObject} fisrt try
+//     [field: string]: { cvalue: number | string | undefined | isBigObject } | undefined;
+// }
+// function summ(a: isBigObject) {
+//     const x = Object.keys(a).map((k) => {
+//         const elem = a[k];
+//         if (elem?.cvalue === undefined) { //"undefined" not undefined
+//             return 2022; // 2022 not "2021"
+//         }
+//         else if (typeof elem.cvalue === 'string') { // string not String
+//             return +elem.cvalue;
+//         }
+//         else if (elem.cvalue instanceof Object) { // elem.cvalue does not have property isBigObject 
+//             return summ(elem.cvalue);
+//         }
+//         return elem.cvalue; // Cyrillic с 
+//     });
+//     let sum = 0;
+//     for (let i = 0; i < x.length; i++) {
+//         sum += +x[i]; // x does not have cvalue property and can be "string" so added +
+//     }
+//     return sum; //return sum not sum
+// }
+// const a1 = { hello: {cvalue: 1}, world: { cvalue: { yay: { cvalue: "2" } } } } //3
+// const a2 = { hello: {cvalue: undefined}, world: { cvalue: { yay: { cvalue: "2" } } } } //2024
+// console.log(summ(a1))
+// console.log(summ(a2))
+// //task 4
+// const students = { "roma": 5, "igor": 2, "maria": 3 };
+// function mapObject<T extends Record<string, Object>, U>(obj: T, transformer: (value: T[keyof T]) => U): Record<keyof T, U> {
+//     const newObject: Record<keyof T, U> = {} as Record<keyof T, U>;
+//     for(const key in obj){
+//         if(obj.hasOwnProperty(key)){
+//             newObject[key] = transformer(obj[key]);
+//         }
+//     }
+//     return newObject;
+// }
+// console.log(mapObject(students, (mark)=> mark > 2))
+// //task 5
+// //simple
+// function funct1<T>(data: Partial<T>, func: (data: Partial<T>) => T): T {
+//     return func(data);
+// }
+// type IdObj<T> = T & { id: string }
+// //advanced 
+// function funct2<T extends IdObj<Partial<T>>>(data: T, func: (data: T) => IdObj<T>): IdObj<T> {
+//     return func(data);
+// }
+// // last task
+// class Rectangle {
+//     w!: number;
+//     h!: number;
+// }
+// class Circle {
+//     radius!: number;
+// }
+// function create<T>(Class: new () => T, count: number) {
+//     let a = []
+//     for (let i = 0; i < count; i++)
+//         a.push(new Class());
+//     return a;
+// }
+// let a: Rectangle[] = create(Rectangle, 10);
+// let b: Circle[] = create(Circle, 20)
+// console.log(a)
+// console.log(b)
 //# sourceMappingURL=index.js.map
